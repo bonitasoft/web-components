7fbd5c94f2af74579cc76c1eccf0fbc624800830
Jerome Cambon <jerome.p.cambon@gmail.com>
2020-01-15 10:33:34 +0100

    Crowdin scripts now handle all the web components at once. List of supported languages are back.

--- components/infrastructure/crowdin/upload.sh
+++ components/infrastructure/crowdin/upload.sh
@@ -4,14 +4,13 @@ usage() {
   command=$(basename $0)
   echo ""
   echo -e "SYNOPSIS"
-  echo -e "    $command --crowdin-api-key=<key> --web-component=<web-component> [--branch=<branch>] [--crowdin-project=<project>] [--upload-translations]"
+  echo -e "    $command --crowdin-api-key=<key> [--branch=<branch>] [--crowdin-project=<project>] [--upload-translations]"
   echo ""
   echo -e "DESCRIPTION"
   echo "  Upload translation keys to crowdin in the specified crowdin project and specified branch"
   echo ""
   echo -e "ARGUMENTS"
   echo -e "  --crowdin-api-key=key    Crowdin api key for the specified project"
-  echo -e "  --web-component          web component name for which files will be uploaded"
   echo ""
   echo -e "OPTIONS"
   echo -e "  --branch                 Crowdin branch on which we want to upload keys (default: current branch)"
@@ -23,9 +22,9 @@ usage() {
 }
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-BASE_DIR=$SCRIPT_DIR/..
+BASE_DIR=$SCRIPT_DIR/../..
 
-PROJECT="bonita-bpm"
+PROJECT="bonita"
 UPLOAD_TRANS=false
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 for i in "$@"; do
@@ -46,10 +45,6 @@ for i in "$@"; do
         PROJECT="${i#*=}"
         shift
         ;;
-        --web-component=*)
-        WC="${i#*=}"
-        shift
-        ;;
          --help)
         usage
         exit 0
@@ -61,24 +56,16 @@ if [ -z "$CROWDINKEY" ]; then
   echo "ERROR: crowdin API key is required";
   usage;
 fi
-if [ -z "$WC" ]; then
-  echo "ERROR: web component name is required";
-  usage;
-fi
-if [ ! -d "$BASE_DIR/packages/$WC" ]; then
-  echo "Error: $BASE_DIR/packages/$WC directory does not exist";
-  usage;
-fi
 
 # $1 files
 # #2 branch
 # $3 export_pattern
 upload_sources() {
   # add file in case it does not exists yet
-  curl --silent -o /dev/null -F "$1" -F "$2" -F "$3" https://api.crowdin.com/api/project/$PROJECT/add-file?key=$CROWDINKEY
+ curl --silent -o /dev/null -F "$1" -F "$2" -F "$3" https://api.crowdin.com/api/project/$PROJECT/add-file?key=$CROWDINKEY
 
   # update file
-  curl -F "$1" -F "$2" -F "$3" https://api.crowdin.com/api/project/$PROJECT/update-file?key=$CROWDINKEY
+ curl -F "$1" -F "$2" -F "$3" https://api.crowdin.com/api/project/$PROJECT/update-file?key=$CROWDINKEY
 }
 
 # $1 directory path
@@ -86,18 +73,21 @@ add_crowdin_directory() {
   curl --silent -o /dev/null -F "name=$1" https://api.crowdin.com/api/project/$PROJECT/add-directory?key=$CROWDINKEY
 }
 
-# $1 target language
+# $1 web component name
+# $2 target language
 upload_translations() {
-  JSON_FILE=packages/$WC/src/i18n/$1.json
+  wc=$1
+  lang=$2
+  JSON_FILE="$BASE_DIR"/packages/$wc/src/i18n/$lang.json
   if [ ! -f "$JSON_FILE" ]; then
     echo "Warning: json file $JSON_FILE does not exist";
     return;
   fi
 
-  echo "Uploading '$WC' $1 translation to '$PROJECT' crowdin project ..."
+  echo "Uploading '$wc' $lang translation to '$PROJECT' crowdin project ..."
 
-  curl -F "files[web-components/$WC/messages.json]=@$JSON_FILE" \
-       -F "language=$1" \
+  curl -F "files[web-components/$wc/messages.json]=@$JSON_FILE" \
+       -F "language=$lang" \
        -F "auto_approve_imported=1" \
        -F "import_duplicates=1" \
        -F "import_eq_suggestions=1" \
@@ -106,23 +96,34 @@ upload_translations() {
 }
 
 echo "***********************************************************************************"
-echo "WEB COMPONENT '$WC' TRANSLATION UPLOAD"
+echo "WEB COMPONENT TRANSLATION UPLOAD"
 echo "***********************************************************************************"
 
 cd "$BASE_DIR" || exit
 
 add_crowdin_directory "$BRANCH_NAME/web-components"
-add_crowdin_directory "$BRANCH_NAME/web-components/$WC"
-echo "Uploading '$WC' web component json translation file to '$PROJECT' crowdin project ..."
-upload_sources \
-    "files[web-components/$WC/messages.json]=@packages/$WC/src/i18n/en.json" \
-    "branch=$BRANCH_NAME" \
-    "exports_pattern=[web-components/$WC/messages.json]=/packages/$WC/src/i18n/%locale%.json"
 
-if [ "$UPLOAD_TRANS" = true ]
-then
-  upload_translations fr
-  upload_translations es
-  upload_translations ja
-  upload_translations pt-BR
-fi
+cd "packages" || exit
+# shellcheck disable=SC2045
+# shellcheck disable=SC2035
+for wcdir in $(ls -d */)
+do
+  # Remove trailer '/'
+  wc=${wcdir%%/}
+  add_crowdin_directory "$BRANCH_NAME/web-components/$wc"
+  echo "Uploading '$wc' web component json translation file to '$PROJECT' crowdin project ..."
+  upload_sources \
+    "files[web-components/$wc/messages.json]=@$wc/src/i18n/en.json" \
+    "branch=$BRANCH_NAME" \
+    "exports_pattern=[web-components/$wc/messages.json]=/$wc/src/i18n/%locale%.json"
+
+  if [ "$UPLOAD_TRANS" = true ]
+  then
+    upload_translations "$wc" fr
+    upload_translations "$wc" es
+    upload_translations "$wc" ja
+    upload_translations "$wc" pt-BR
+  fi
+
+done
+
