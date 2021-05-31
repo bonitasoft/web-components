c3f51a78822ec976042cd5229b5dcba92e1a3fdf
Benjamin Parisel <benjamin.parisel@bonitasoft.com>
2021-05-31 11:48:03 +0200

    feat(switch): Update Switch widget (#54)
    * feat(switch): Update Switch widget
    
    * return from type mapping if possible
    * Convert primitive value to String value if needed
    * Disabled option if mapping is incompatible
    * Adding type validation on constant mapping
    * Adapt Test
    
    Covers [UID-369](https://bonitasoft.atlassian.net/browse/UID-369)

--- legacy-elements/bo-switch-config/test/mapping-property.test.js
+++ legacy-elements/bo-switch-config/test/mapping-property.test.js
@@ -1,4 +1,4 @@
-import mapping from '../mapping-property';
+import {mapping, isIncompatibleMapping} from '../mapping-property';
 
 describe('Mapping function', () => {
   it('should return value of from widget when all properties is mapping', () => {
@@ -98,4 +98,86 @@ describe('Mapping function', () => {
     };
     expect(res).toEqual(expected);
   });
+
+  it('should return correct type mapping when from property is used for an other to property', () => {
+    let from = {
+      cssClasses: { type: 'constant', value: 'well jumbotron', label: 'CSS classes' },
+      label: { type: 'interpolation', value: 'Default label', label: 'Label' },
+      placeholder: { type: 'interpolation', value: 'My Placeholder', label: 'Placeholder' },
+      value: { type: 'variable', value: 'user', label: 'Value' },
+      pageSize: { type: 'constant', value: 10, label: 'Page Size' }
+    };
+
+    let to = {
+      cssClasses: { bond: 'constant', value: '', label: 'CSS classes' },
+      label: { bond: 'interpolation', value: 'This is my label', label: 'Label' },
+      value: { bond: 'variable', value: '', label: 'Value' },
+      pageSize: { bond: 'variable', value: '10', label: 'Page Size' }
+
+    };
+
+    let optionsSelected = [
+      { selected: 'cssClasses', key: 'cssClasses' },
+      { selected: 'placeholder', key: 'label' },
+      { selected: 'placeholder', key: 'value' },
+      { selected: 'pageSize', key: 'pageSize' }
+    ];
+
+    let res = mapping(from, to, optionsSelected);
+    let expected = {
+      cssClasses: { type: 'constant', value: 'well jumbotron' },
+      label: { type: 'interpolation', value: 'My Placeholder' },
+      value: { type: 'variable', value: 'My Placeholder' },
+      pageSize: { type: 'variable', value: '10' }
+    };
+    expect(res).toEqual(expected);
+  });
+  it('should return correct value type for value when input is a number an output a variable', () => {
+    let from = {
+      startPage: { type: 'constant', value: 1, label: 'Start page' },
+      pageSize: { type: 'constant', value: 10, label: 'Page Size' }
+    };
+
+    let to = {
+      startPage: { bond: 'variable', value: '', label: 'Start page'},
+      pageSize: { bond: 'variable', value: '10', label: 'Page Size' }
+
+    };
+
+    let optionsSelected = [
+      { selected: 'startPage', key: 'startPage' },
+      { selected: 'pageSize', key: 'pageSize' }
+    ];
+
+    let res = mapping(from, to, optionsSelected);
+    let expected = {
+      startPage: { type: 'variable', value: '1' },
+      pageSize: { type: 'variable', value: '10' }
+    };
+    expect(res).toEqual(expected);
+  });
+});
+
+describe('isIncompatibleMapping function', () => {
+  it('should be check if two simple mapping type is incompatible', () => {
+    expect(isIncompatibleMapping({bond:'variable'}, {type:'interpolation'})).toEqual(true);
+    expect(isIncompatibleMapping({bond:'constant'}, {type:'interpolation'})).toEqual(true);
+    expect(isIncompatibleMapping({bond:'constant'}, {type:'expression'})).toEqual(true);
+    expect(isIncompatibleMapping({bond:'constant'}, {type:'constant'})).toEqual(false);
+    expect(isIncompatibleMapping({bond:'constant'}, {type:'variable'})).toEqual(true);
+  });
+  it('should be check if two constant mapping is incompatible', () => {
+    expect(isIncompatibleMapping({bond:'constant',type:'integer'}, {type:'constant', value:'true'})).toEqual(true);
+    expect(isIncompatibleMapping({bond:'constant',type:'integer'}, {type:'constant', value:'aStringValue'})).toEqual(true);
+    expect(isIncompatibleMapping({bond:'constant',type:'boolean'}, {type:'constant', value:'true'})).toEqual(true);
+    expect(isIncompatibleMapping({bond:'constant',type:'boolean'}, {type:'constant', value:'false'})).toEqual(true);
+  });
+
+  it('should be check if two constant mapping is compatible', () => {
+    expect(isIncompatibleMapping({bond:'constant',type:'integer'}, {type:'constant', value:2})).toEqual(false);
+    expect(isIncompatibleMapping({bond:'constant',type:'integer'}, {type:'constant', value:'2'})).toEqual(false);
+    expect(isIncompatibleMapping({bond:'constant',type:'boolean'}, {type:'constant', value:true})).toEqual(false);
+    expect(isIncompatibleMapping({bond:'constant',type:'boolean'}, {type:'constant', value:false})).toEqual(false);
+  });
+
 });
